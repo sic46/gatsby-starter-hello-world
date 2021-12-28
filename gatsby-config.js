7d12430176966113ffa4e28ec0cdc72adfafaf2a
sic46 <66070130+sic46@users.noreply.github.com>
2021-12-28 18:01:57 +1100

    Revert "Update gatsby-config.js"
    This reverts commit 74e82200562017abc70ce803474b4bb68788afbc.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,18 +6,5 @@
 
 module.exports = {
   /* Your site config here */
-   siteMetadata: {
-    title: "Winston's Recipes",
-    description: "A collection of my own recipes",
-    author: "@Winston",
- 
-    ],
-  },
-  
-  plugins: [
-  `gatsby-plugin-styled-components`,
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-  ],
+  plugins: [],
 }
