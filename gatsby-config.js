a60f7d9e39fbc181bfa5866932f6d38a78b2e048
Sic46 <66070130+sic46@users.noreply.github.com>
2022-01-09 12:20:23 +1100

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,33 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`
+    ,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/pages/assets/images/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+        // Add any options here
+      },
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+        // Add any options here
+      },
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `kt64b8nliim2`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_API_KEY,
+    },
+    },
+  ],
 }
