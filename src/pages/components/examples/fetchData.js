3b1442fc63c619048461861590db87031b1ac02a
Sic46 <66070130+sic46@users.noreply.github.com>
2022-01-12 18:53:38 +1100

    update fetch

--- src/pages/components/examples/fetchData.js
+++ src/pages/components/examples/fetchData.js
@@ -1,24 +1,7 @@
 import React from 'react'
 import { useStaticQuery,graphql} from 'gatsby'
-const getData = graphql`
-{
-    site {
-      info: siteMetadata {
-        title
-        simpleData
-        person {
-          age
-          name
-        }
-        complexData {
-          age
-          name
-        }
-        author
-      }
-    }
-  }
-`
+const getData = graphql
+
 const FetchData = () => {
     const data = useStaticQuery(getData);
     return (
