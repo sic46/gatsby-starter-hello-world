6257e7b3f303f4b177476c205408875cc3564e4e
Sic46 <66070130+sic46@users.noreply.github.com>
2022-01-12 18:49:43 +1100

    testing update

--- src/pages/testing.js
+++ src/pages/testing.js
@@ -1,32 +1,11 @@
 import React from 'react'
-import { graphql } from 'gatsby'
-const Testing = ({data}) => {
-        return (
-        <div>
-            <h2>
-                Testing {data.site.info.title}
-            </h2>
-            
-        </div>
-    )
-} 
-export const data = graphql`
-{
-    site {
-      info: siteMetadata {
-        title
-        simpleData
-        person {
-          age
-          name
-        }
-        complexData {
-          age
-          name
-        }
-        author
-      }
-    }
-  }
-  `
-export default Testing
+
+const testing = () => {
+  return (
+    <div>
+      <h2>Test</h2>
+    </div>
+  )
+}
+
+export default testing
