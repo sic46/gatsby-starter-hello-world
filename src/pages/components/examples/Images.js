302356f5de591f159adaa9e8e9b4b52075dcf37b
Sic46 <66070130+sic46@users.noreply.github.com>
2022-01-13 19:50:55 +1100

    images

--- src/pages/components/examples/Images.js
+++ src/pages/components/examples/Images.js
@@ -5,29 +5,30 @@ import { StaticImage } from "gatsby-plugin-image"
 const Images = () => {
   return (
     <Wrapper>
-      <StaticImage src=".../src/assets/images/recipes/big.jpg"
-      alt="food"
-      placeholder="tracedSVG"
-      layout="fullwidth"
-      className="example-img"
-      as="section"
+      <StaticImage
+        src=".../src/assets/images/recipes/big.jpg"
+        alt="food"
+        placeholder="tracedSVG"
+        layout="fullwidth"
+        className="example-img"
+        as="section"
       />
-      </Wrapper>
+    </Wrapper>
   )
 }
-      const Wrapper = styled.section`
-      width: 70vw;
-      margin: 0 auto;
-      display: grid;
-      text-align: center;
-      gap:2rem;
+const Wrapper = styled.section`
+  width: 70vw;
+  margin: 0 auto;
+  display: grid;
+  text-align: center;
+  gap: 2rem;
 
-      article {
-        border: 2px solid red;
-      }
-      .example-img{
-        border-radius: 1rem;
-      }
-    `
+  article {
+    border: 2px solid red;
+  }
+  .example-img {
+    border-radius: 1rem;
+  }
+`
 
 export default Images
