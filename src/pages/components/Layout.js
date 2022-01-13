6fe5a046120806afab8cc941a94ad3def4e3d1b8
Sic46 <66070130+sic46@users.noreply.github.com>
2022-01-13 19:56:52 +1100

    layout

--- src/pages/components/Layout.js
+++ src/pages/components/Layout.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
+import React from "react"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
 
-import '../assets/css/normalize.css'
-import '../assets/css/style.css'
+import "../assets/css/normalize.css"
+import "../assets/css/style.css"
 
-
-const Layout = ({children}) => {
-    return (
-        <>
-            <Navbar />
-                {children}
-            <Footer />
-        </>
-    )
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  )
 }
 
 export default Layout
