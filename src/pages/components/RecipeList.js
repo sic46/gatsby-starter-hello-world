07289fe43f08a2cbfea583b7aae62aa05a5fa972
Sic46 <66070130+sic46@users.noreply.github.com>
2022-01-13 19:45:46 +1100

    fetchdata

--- src/pages/components/RecipeList.js
+++ src/pages/components/RecipeList.js
@@ -1,26 +1,32 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import React from "react"
+import { Link } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-const RecipeList = ({recipes=[]}) => {
-    return (
+const RecipeList = ({ recipes = [] }) => {
+  return (
     <div className="recipes-list">
-        {recipes.map(recipe=>{
-            const {id,title,image,prepTime,cookTime}= recipe;
-            const pathToImage = getImage(image)
-            
-            return(
-            <Link key={id} to={`/${title}`} className="recipe">
-            <GatsbyImage image={pathToImage} className="recipe-img" alt={title}/>
-            
+      {recipes.map(recipe => {
+        const { id, title, image, prepTime, cookTime } = recipe
+        const pathToImage = getImage(image)
+
+        return (
+          <Link key={id} to={`/${title}`} className="recipe">
+            <GatsbyImage
+              image={pathToImage}
+              className="recipe-img"
+              alt={title}
+            />
+
             <h5>{title}</h5>
-            <p> Prep: {prepTime}min | Cook time: {cookTime}min</p></Link>
+            <p>
+              {" "}
+              Prep: {prepTime}min | Cook time: {cookTime}min
+            </p>
+          </Link>
         )
-        })}
-            
-        </div>
-    )
+      })}
+    </div>
+  )
 }
 
 export default RecipeList
-
--- src/pages/components/examples/fetchData.js
+++ src/pages/components/examples/fetchData.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import { useStaticQuery,graphql} from 'gatsby'
-const getData = graphql
-
+const getData = graphql`
+{
+    site {
+      info: siteMetadata {
+        title
+        simpleData
+        person {
+          age
+          name
+        }
+        complexData {
+          age
+          name
+        }
+        author
+      }
+    }
+  }
+`
 const FetchData = () => {
     const data = useStaticQuery(getData);
     return (
